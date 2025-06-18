9ab9d4168ed5c76cc1582591fbb1af1d188e5a20
BrendanA1622 <89553639+BrendanA1622@users.noreply.github.com>
2025-06-17 23:50:13 -0700

    First commit

---
+++ main.lua
@@ -0,0 +1,28 @@
+function love.load()
+    player = {}
+    player.x = 400
+    player.y = 200
+
+    player.speed = 300
+end
+
+function love.update(dt)
+    player.x = player.x + player.speed * dt * (int(love.keyboard.isDown("right")) - int(love.keyboard.isDown("left")))
+    player.y = player.y + player.speed * dt * (int(love.keyboard.isDown("down")) - int(love.keyboard.isDown("up")))
+end
+
+function love.draw()
+    love.graphics.circle("fill", player.x, player.y, 50)
+end
+
+
+
+function int(value)
+    return value and 1 or 0
+end
+
+function love.keypressed(key, unicode)
+	if key == "f8" then
+		love.event.quit()
+	end
+end
